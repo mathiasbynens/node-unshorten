f4bc78bf9bfb9c28abfba2f2de8b0d1b2f3c0590
Mathias Bynens <mathias@qiwi.be>
2011-09-29 15:15:18 +0200

    Tag the 0.0.1 release.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Unshorten
+
+A simple URL unshortener for [Node.js](http://nodejs.org/).
+
+## Example usage
+
+```js
+var unshorten = require('./unshorten.js');
+unshorten.expand('http://mths.be/unshorten', function(url) {
+	console.log(url + ' is where it’s at!');
+});
+```
+
+## Credits
+
+Made for fun (and to get more familiar with Node) by [Mathias](http://mathiasbynens.be/).
---
+++ unshorten.js
@@ -0,0 +1,29 @@
+(function() {
+
+	var urlLibrary = require('url'),
+	    http = require('http'),
+	    https = require('https');
+
+	function unshorten(url, callback) {
+		var request;
+		url = urlLibrary.parse(url);
+		request = ('https' == url.protocol ? https : http).request(
+			{
+				'headers': 't.co' == url.host ? {} : { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_1) AppleWebKit/535.5 (KHTML, like Gecko) Chrome/16.0.894.0 Safari/535.5' },
+				'method': 'HEAD',
+				'host': url.host,
+				'path': url.pathname
+			},
+			function(response) {
+				if (~[301, 302].indexOf(response.statusCode)) {
+					(callback || console.log)(response.headers.location);
+					return;
+				}
+			}
+		);
+		request.end();
+	}
+
+	exports.expand = unshorten;
+
+}());
